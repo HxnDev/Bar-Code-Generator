7af19744cfb1080158f4c8410b1b6d3410b1a3bb
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2021-09-05 18:53:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+
+
 from barcode import EAN13   # Install using "pip install python-barcode" and "pip install pillow"
 from barcode.writer import ImageWriter
 
 num = input("Enter the number whose barcode you want to generate = ")
 code = EAN13(num, writer=ImageWriter())     # Generating barcode
 code.save("barcode")    # Saving as png image
+
+
